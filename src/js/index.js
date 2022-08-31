8701a8d505e0f1c0ffd3b9b6394bbdb79701fef7
freeivanzw <freeivanzw@gmail.com>
2022-08-31 18:04:01 +0300

    start header

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
-console.log('hello world1')
-
-$('.test_slider').slick()
+$(function () {
+    if(window.innerWidth < 1024) {
+        $('.input-wrap').children(['input', 'textarea']).on('focus', function () {
+            $(this).parent('.input-wrap').addClass('active');
+        })
+        $('.input-wrap').children(['input', 'textarea']).on('focusout', function () {
+            $(this).parent('.input-wrap').removeClass('active');
+        })
+    }
+})
