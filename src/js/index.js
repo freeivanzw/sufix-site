167a7931493b080495bbbb06b27612ff3ae95123
freeivanzw <freeivanzw@gmail.com>
2022-09-01 18:19:01 +0300

    header footer

--- src/js/index.js
+++ src/js/index.js
@@ -7,4 +7,30 @@ $(function () {
             $(this).parent('.input-wrap').removeClass('active');
         })
     }
+
+    $('.mob_menu-open').on('click', function (e) {
+        e.preventDefault();
+
+        $('.mob_menu').addClass('active');
+    })
+
+    $('.mob_menu-close').on('click', function (e) {
+        e.preventDefault();
+
+        $('.mob_menu').removeClass('active');
+    })
+
+    $('.mob_top-menu').find('a').each(function (index, element) {
+       if($(element).next('ul').length != 0) {
+           $(this).parent('li').addClass('has_drop');
+
+           $('.has_drop').on('click', 'a', function (e) {
+               if($(this).closest('li').is('.has_drop')) {
+                   e.preventDefault();
+                   $(this).closest('li').toggleClass('menu_opened');
+               }
+           })
+       }
+    })
+
 })
